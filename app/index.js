925246b962ae224cf2f8baaef1a423ab8e73052f
David Ahn <dja1990@gmail.com>
2018-09-12 14:50:26 -0700

    in class ES6 Class practices

--- app/index.js
+++ app/index.js
@@ -1,9 +1,18 @@
-import addToDom from './add_to_dom';
-import { subtract } from './math';
+// import addToDom from './add_to_dom';
+// import { subtract } from './math';
 
 // import str, { subtract } from './math';
 
-console.log('Math:', subtract);
+// console.log('Math:', subtract);
 
-addToDom('h1', `52 - 10 = ${subtract(52, 10)}`);
+// addToDom('h1', `52 - 10 = ${subtract(52, 10)}`);
 
+/////////////////////////////////////////////////////////////////////////////////
+
+import SuperPerson from './person';
+
+const bob = new SuperPerson('Bob', 'Cleaning Windows');;
+
+bob.greet();
+
+bob.saySuperPower('Cleaning Windows');
